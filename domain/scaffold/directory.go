ed6dc3a926fd7d0887e8fe201be4a886f0a2fd1f
izumin5210 <masayuki@izumin.info>
2017-07-22 13:56:03 +0900

    Make scaffold.NewDirectory construct descendant entries

--- domain/scaffold/directory.go
+++ domain/scaffold/directory.go
@@ -3,12 +3,29 @@ package scaffold
 // Directory represents a directory entry of filesystem
 type Directory struct {
 	Entry
-	entries []Entry
+	children map[string]Entry
 }
 
 // NewDirectory returns a directory object
 func NewDirectory(path string, entries []Entry) *Directory {
-	return &Directory{Entry: &entry{path: path}, entries: entries}
+	entry := &entry{path: path}
+	children := map[string]Entry{}
+	descendants := []Entry{}
+	for _, e := range entries {
+		if entry.IsParentOf(e) {
+			children[e.Name()] = e
+		} else {
+			descendants = append(descendants, e)
+		}
+	}
+	for n, e := range children {
+		if e.IsDir() {
+			children[n] = NewDirectory(e.Path(), descendants)
+		} else {
+			children[n] = e
+		}
+	}
+	return &Directory{Entry: entry, children: children}
 }
 
 // EmptyDirectory returns an empty directory object
@@ -21,7 +38,7 @@ func (d *Directory) IsDir() bool {
 	return true
 }
 
-// Entries returns child entries of this directory
-func (d *Directory) Entries() []Entry {
-	return d.entries
+// Children returns child entries of this directory
+func (d *Directory) Children() map[string]Entry {
+	return d.children
 }
