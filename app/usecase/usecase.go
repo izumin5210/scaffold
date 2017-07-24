2c8995d3079c9d8b1c48627a74ce641242baf417
izumin5210 <masayuki@izumin.info>
2017-07-24 11:33:35 +0900

    Generate usecase mocks

--- app/usecase/usecase.go
+++ app/usecase/usecase.go
@@ -1,1 +1,4 @@
+//go:generate mockgen -package=usecase -source=get_scaffolds.go -destination=get_scaffolds_mock.go
+//go:generate mockgen -package=usecase -source=create_scaffold.go -destination=create_scaffold_mock.go
+
 package usecase
