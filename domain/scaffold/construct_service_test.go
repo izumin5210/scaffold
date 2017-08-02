4b5653535ba6c33778398112a728bc2ae2daf482
izumin5210 <masayuki@izumin.info>
2017-08-02 23:57:13 +0900

    Fix ConstructService test

--- domain/scaffold/construct_service_test.go
+++ domain/scaffold/construct_service_test.go
@@ -1,6 +1,7 @@
 package scaffold
 
 import (
+	"fmt"
 	"path/filepath"
 	"reflect"
 	"testing"
@@ -36,7 +37,7 @@ func Test_ConstructService(t *testing.T) {
 	ctx := newConstructServiceTestContext(t)
 	defer ctx.ctrl.Finish()
 
-	sc := NewScaffold(filepath.Join(ctx.root, ".scaffold", "foo"), &Meta{})
+	sc := NewScaffold(filepath.Join(ctx.root, ".scaffold", "golang"), &Meta{})
 	name := "gopher"
 
 	getTmplPath := func(relpath string) TemplateString {
@@ -47,19 +48,95 @@ func Test_ConstructService(t *testing.T) {
 	}
 
 	cases := []struct {
-		tmpls     []TemplateEntry
-		values    interface{}
-		existings map[string]ConcreteEntry
-		calls     map[string]*constructCallbackCall
+		tmpls             []TemplateEntry
+		values            interface{}
+		existings         map[string]ConcreteEntry
+		createdWithParent map[string]bool
+		calls             map[string]*constructCallbackCall
+		overwriting       map[string]struct{}
 	}{
 		{},
 		{
 			tmpls: []TemplateEntry{
-				NewTemplateFile(getTmplPath("bar.go"), "package main"),
+				NewTemplateFile(getTmplPath("foo.go"), "package main", sc.Path()),
+			},
+			createdWithParent: map[string]bool{
+				getConcPath("foo.go"): false,
 			},
 			values: struct{ Name string }{Name: name},
 			calls: map[string]*constructCallbackCall{
-				getConcPath("bar.go"): &constructCallbackCall{status: ConstructSuccess},
+				getConcPath("foo.go"): &constructCallbackCall{status: ConstructSuccess},
+			},
+		},
+		// ├── foo
+		// │   ├── qux
+		// │   │   └── corge.go
+		// │   └── quux
+		// │       └── grault.go
+		// ├── bar
+		// └── baz
+		// │   ├── {{name}}
+		// │   │   ├── garlpy.go
+		// │   │   └── {{name}}.go
+		// │   ├── {{name}}.go
+		// │   └── {{name}}_test.go
+		// └── meta.toml
+		// └── waldo.go
+		{
+			tmpls: []TemplateEntry{
+				NewTemplateDir(getTmplPath("bar"), sc.Path()),
+				NewTemplateFile(getTmplPath("baz/{{name}}.go"), "package baz", sc.Path()),
+				NewTemplateFile(getTmplPath("baz/{{name}}_test.go"), "package baz", sc.Path()),
+				NewTemplateFile(getTmplPath("baz/{{name}}/garlpy.go"), "package {{name}}", sc.Path()),
+				NewTemplateFile(getTmplPath("baz/{{name}}/{{name}}.go"), "package {{name}}", sc.Path()),
+				NewTemplateFile(getTmplPath("foo/quux/grault.go"), "package quux", sc.Path()),
+				NewTemplateFile(getTmplPath("foo/qux/corge.go"), "package qux", sc.Path()),
+				NewTemplateFile(getTmplPath("waldo.go"), "package main", sc.Path()),
+			},
+			createdWithParent: map[string]bool{
+				getConcPath("bar"):                                   false,
+				getConcPath(fmt.Sprintf("baz/%s_test.go", name)):     false,
+				getConcPath(fmt.Sprintf("baz/%s/garlpy.go", name)):   true,
+				getConcPath(fmt.Sprintf("baz/%s/%s.go", name, name)): false,
+				getConcPath("foo/quux/grault.go"):                    true,
+				getConcPath("waldo.go"):                              false,
+			},
+			values: struct{ Name string }{Name: name},
+			existings: map[string]ConcreteEntry{
+				// existed
+				getConcPath("foo/qux/corge.go"): NewConcreteFile(
+					getConcPath("foo/qux/corge.go"),
+					"package qux",
+					string(getTmplPath("foo/qux/corge.go")),
+				),
+				// conflicted
+				getConcPath(fmt.Sprintf("baz/%s.go", name)): NewConcreteFile(
+					getConcPath(fmt.Sprintf("baz/%s.go", name)),
+					"",
+					string(getTmplPath("baz/{{name}}.go")),
+				),
+				// conflicted and will overwrite
+				getConcPath(fmt.Sprintf("baz/%s_test.go", name)): NewConcreteFile(
+					getConcPath(fmt.Sprintf("baz/%s_test.go", name)),
+					"",
+					string(getTmplPath("baz/{{name}}_test.go")),
+				),
+			},
+			calls: map[string]*constructCallbackCall{
+				getConcPath("bar"):                                   &constructCallbackCall{dir: true, status: ConstructSuccess},
+				getConcPath("baz"):                                   &constructCallbackCall{dir: true, status: ConstructSkipped},
+				getConcPath(fmt.Sprintf("baz/%s.go", name)):          &constructCallbackCall{conflicted: true, status: ConstructSkipped},
+				getConcPath(fmt.Sprintf("baz/%s_test.go", name)):     &constructCallbackCall{conflicted: true, status: ConstructSuccess},
+				getConcPath(fmt.Sprintf("baz/%s", name)):             &constructCallbackCall{dir: true, status: ConstructSuccess},
+				getConcPath(fmt.Sprintf("baz/%s/garlpy.go", name)):   &constructCallbackCall{status: ConstructSuccess},
+				getConcPath(fmt.Sprintf("baz/%s/%s.go", name, name)): &constructCallbackCall{status: ConstructSuccess},
+				getConcPath("foo/quux"):                              &constructCallbackCall{dir: true, status: ConstructSuccess},
+				getConcPath("foo/quux/grault.go"):                    &constructCallbackCall{status: ConstructSuccess},
+				getConcPath("foo/qux/corge.go"):                      &constructCallbackCall{status: ConstructSkipped},
+				getConcPath("waldo.go"):                              &constructCallbackCall{status: ConstructSuccess},
+			},
+			overwriting: map[string]struct{}{
+				getConcPath(fmt.Sprintf("baz/%s_test.go", name)): struct{}{},
 			},
 		},
 	}
@@ -71,16 +148,28 @@ func Test_ConstructService(t *testing.T) {
 			GetConcreteEntries(sc, c.tmpls, c.values).
 			Return(c.existings, nil)
 
+		for _, tmpl := range c.tmpls {
+			conc, _ := tmpl.Compile(ctx.root, c.values)
+			if dirCreated, ok := c.createdWithParent[conc.Path()]; ok {
+				ctx.repo.EXPECT().Create(conc).Return(true, dirCreated, nil)
+			}
+		}
+
 		calls := map[string]*constructCallbackCall{}
 
 		err := ctx.service.Perform(
+			ctx.root,
 			sc,
 			c.values,
 			func(path string, dir, conflicted bool, status ConstructStatus) {
 				calls[path] = &constructCallbackCall{dir: dir, conflicted: conflicted, status: status}
 			},
 			func(path, oldContent, newContent string) bool {
-				return false
+				if oldContent == newContent {
+					t.Errorf("%s is conflicted but old one and new one has same content", path)
+				}
+				_, ok := c.overwriting[path]
+				return ok
 			},
 		)
 
