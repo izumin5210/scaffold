5ddb286245ba4a56ec1439c7cfd009a43bf7e980
izumin5210 <masayuki@izumin.info>
2017-08-03 14:48:28 +0900

    Add first implementation for ConstructService

--- domain/scaffold/construct_service.go
+++ domain/scaffold/construct_service.go
@@ -1,9 +1,5 @@
 package scaffold
 
-import (
-	"github.com/pkg/errors"
-)
-
 // ConstructService creates directories and files from scaffold templates
 type ConstructService interface {
 	Perform(
@@ -33,5 +29,51 @@ func (s *constructService) Perform(
 	cb ConstructCallback,
 	cCb ConstructConflictedCallback,
 ) error {
-	return errors.New("errors")
+	// TODO: Should handle errors
+	tmpls, _ := s.repo.GetTemplates(sc)
+	// TODO: Should handle errors
+	exConcs, _ := s.repo.GetConcreteEntries(sc, tmpls, v)
+
+	createdDirs := map[string]struct{}{}
+
+	for _, tmpl := range tmpls {
+		// TODO: Should handle errors
+		conc, _ := tmpl.Compile(rootPath, v)
+		conflicted := false
+
+		if exConc, ok := exConcs[conc.Path()]; ok {
+			if conc.IsDir() {
+				cb(conc.Path(), true, false, ConstructSkipped)
+				continue
+			}
+			if got, want := exConc.Content(), conc.Content(); got == want {
+				cb(conc.Path(), false, false, ConstructSkipped)
+				continue
+			} else {
+				if cCb(conc.Path(), got, want) {
+					conflicted = true
+				} else {
+					cb(conc.Path(), false, true, ConstructSkipped)
+					continue
+				}
+			}
+		}
+
+		// TODO: Should handle errors
+		created, dirCreated, _ := s.repo.Create(conc)
+		if _, alreadyCreated := createdDirs[conc.Dir()]; conc.Dir() != rootPath && !alreadyCreated {
+			status := ConstructSuccess
+			if !dirCreated {
+				status = ConstructSkipped
+			}
+			cb(conc.Dir(), true, false, status)
+			createdDirs[conc.Dir()] = struct{}{}
+		}
+		status := ConstructSuccess
+		if !created {
+			status = ConstructSkipped
+		}
+		cb(conc.Path(), conc.IsDir(), conflicted, status)
+	}
+	return nil
 }
